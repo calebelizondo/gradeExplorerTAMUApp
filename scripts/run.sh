122c74f6e19cd2694331b03eef2d3991a2343ed9
Caleb Elizondo <calebelizondo@tamu.edu>
2023-10-14 20:56:30 -0500

    removed import sections

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,4 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py makemigrations
-python manage.py migrate
-python manage.py import_sections sections.csv
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
