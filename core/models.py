ba64c9129975f243e11d16ac9aa3fc0608f04676
Caleb Elizondo <calebelizondo@tamu.edu>
2023-11-14 21:36:42 -0600

    updated views

--- core/models.py
+++ core/models.py
@@ -8,7 +8,8 @@ class Section_grades(models.Model):
         ('FALL', 'Fall'),
         ('WINTER', 'Winter'),
     ]
-    #section information
+    
+    # section information
     semester = models.CharField(max_length=10, choices=SEMESTER_CHOICES)
     year = models.PositiveIntegerField()
     subject_code = models.CharField(max_length=4)
@@ -16,7 +17,7 @@ class Section_grades(models.Model):
     section_code = models.CharField(max_length=4)
     professor_name = models.CharField(max_length=100)
 
-    #grades
+    # grades
     a_count = models.PositiveIntegerField()
     b_count = models.PositiveIntegerField()
     c_count = models.PositiveIntegerField()
@@ -28,42 +29,46 @@ class Section_grades(models.Model):
     u_count = models.PositiveIntegerField()
     x_count = models.PositiveIntegerField()
 
-    #course evaluation responses
+    # course evaluation responses
     answers_one = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     answers_two = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     answers_three = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     answers_four = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     answers_five = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     answers_six = ArrayField(
         models.IntegerField(),
-        blank=True, 
-        null=False, 
+        blank=True,
+        null=False,
     )
 
     def __str__(self):
         return f"{self.year} {self.semester} - {self.course_code} ({self.section_code})"
+
+    class Meta:
+        # Define a unique constraint for the combination of fields
+        unique_together = ['year', 'semester', 'subject_code', 'course_code', 'section_code']
