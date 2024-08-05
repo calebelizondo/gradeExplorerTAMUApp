48987f16d4a9b4a9d89a2745724231362120b7a0
Caleb Elizondo <calebelizondo@tamu.edu>
2024-08-05 18:26:37 -0500

    added post message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Dashboard';
 
 function App() {
+
+  //needs to sendHeight to dynamically size parent iframe on embed
+  useEffect(() => {
+    const sendHeight = () => {
+      const height = document.documentElement.scrollHeight;
+      window.parent.postMessage({ type: 'SET_HEIGHT', height }, '*');
+    };
+
+    sendHeight();
+    window.addEventListener('resize', sendHeight);
+
+    return () => window.removeEventListener('resize', sendHeight);
+  }, []);
+
   return (
     <Dashboard />
   );
