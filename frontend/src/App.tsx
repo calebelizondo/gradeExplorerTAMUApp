fd7721d7a67d985db72174c3070c5ef86b74ff4e
Caleb Elizondo <calebelizondo@tamu.edu>
2025-01-04 16:38:00 -0600

    made frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <p>Hello World!</p>
+  );
+}
+
+export default App;
